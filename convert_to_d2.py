ccdd9434e9452ac6df99a78108a68aa4c4547a82
Hu Ye <xiaohuzc@gmail.com>
2021-12-05 16:57:24 +0800

    Update convert_to_d2.py

--- convert_to_d2.py
+++ convert_to_d2.py
@@ -6,8 +6,11 @@ import torch
 def parse_args():
     parser = argparse.ArgumentParser("D2 model converter")
 
-    parser.add_argument("--source_model", default="", type=str, help="Path or url to the  model to convert")    parser.add_argument("--output_model", default="", type=str, help="Path where to save the converted model")
+    parser.add_argument("--source_model", default="", type=str, help="Path or url to the  model to convert")
+    parser.add_argument("--output_model", default="", type=str, help="Path where to save the converted model")
     return parser.parse_args()
+
+
 def main():
     args = parse_args()
     
